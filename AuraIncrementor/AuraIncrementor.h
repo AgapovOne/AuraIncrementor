1b955e4012b98e6d2f26033159393f89b9262789
Alex Agapov <agapov.one@gmail.com>
2019-02-27 20:34:38 +0500

    Initial Commit

---
+++ AuraIncrementor/AuraIncrementor.h
@@ -0,0 +1,19 @@
+//
+//  AuraIncrementor.h
+//  AuraIncrementor
+//
+//  Created by Alex Agapov on 27/02/2019.
+//  Copyright © 2019 AgapovOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AuraIncrementor.
+FOUNDATION_EXPORT double AuraIncrementorVersionNumber;
+
+//! Project version string for AuraIncrementor.
+FOUNDATION_EXPORT const unsigned char AuraIncrementorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuraIncrementor/PublicHeader.h>
+
+
